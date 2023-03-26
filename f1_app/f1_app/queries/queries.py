992b5e8b6333b05740e8b15d8f192e7c2cd1f1db
bastos-01 <p.bastos112@gmail.com>
2023-03-26 22:53:20 +0100

    queries

--- f1_app/f1_app/queries/queries.py
+++ f1_app/f1_app/queries/queries.py
@@ -15,11 +15,12 @@ def get_pilot_info(name):
     query = """
     PREFIX driver: <http://f1/driver/pred/> 
 
-    SELECT ?driver_id ?forename ?surname WHERE
+    SELECT ?driver_id ?forename ?surname ?nationality WHERE
     {
     {
-    SELECT DISTINCT ?driver_id ?forename ?surname
+    SELECT DISTINCT ?driver_id ?forename ?surname ?nationality
     WHERE{
+        ?driver_id driver:nationality ?nationality.
         ?driver_id driver:forename ?forename.
         ?driver_id driver:surname ?surname.
         FILTER regex(?forename, "DRIVER_NAME" , "i")
@@ -27,8 +28,9 @@ def get_pilot_info(name):
     }
     UNION
     {
-    SELECT DISTINCT ?driver_id ?forename ?surname 
+    SELECT DISTINCT ?driver_id ?forename ?surname ?nationality
     WHERE{
+        ?driver_id driver:nationality ?nationality.
         ?driver_id driver:forename ?forename.
         ?driver_id driver:surname ?surname.
         FILTER regex(?surname, "DRIVER_NAME" , "i")
@@ -41,13 +43,26 @@ def get_pilot_info(name):
     query = query.replace("DRIVER_NAME", name)
 
     payload_query = {"query": query}
-    res = accessor.sparql_select(body=payload_query, repo_name=repo)
-    print(res)
-    res = json.loads(res)
+    response = accessor.sparql_select(body=payload_query, repo_name=repo)
+
+    response = json.loads(response)
+
+    if response['results']['bindings']:
+        data = response['results']['bindings'][0]
+        print([data['driver_id']['value'].split('/')[-1], data['forename']['value'], data['surname']['value'], data['nationality']['value']])
+        return data
+    else:
+        return []
+    
+
+    
+        
+
+
 
     
 
-get_pilot_info("Lewis")
+get_pilot_info("Hamilton")
 #
     #all_peoplpe = [(e["p"]["value"], e["name"]["value"]) for e in res['results']['bindings']]
 #
