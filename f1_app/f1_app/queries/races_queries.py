1d9b1d046bf12f158a71db11bb81cb3d4ff51aaa
bastos-01 <p.bastos112@gmail.com>
2023-03-28 17:14:42 +0100

    more races queries

--- f1_app/f1_app/queries/races_queries.py
+++ f1_app/f1_app/queries/races_queries.py
@@ -70,12 +70,8 @@ def races_by_season(season):
 
     Returns
     -------
-    dict :
-        {
-        
-        }
-      list(tuples)
-        [(race_id, race_name, season, round)]
+    list : tuples
+        [(driver_code, forename, surname, nationality, season, race_name, round, grid, status, position, points)]
     """
 def pilot_standings_races_by_season(code, season):
     query = """
@@ -135,7 +131,80 @@ def pilot_standings_races_by_season(code, season):
 
 
 
+""" Get all pilots standings for a specific race for a specific season
+
+    Parameters
+    ----------
+    name : str
+        Race name
+    season: int
+        Season year
+
+    Returns
+    -------
+    list : tuples
+        [(driver_code, forename, surname, nationality, season, race_name, round, grid, status, position, points)]
+    """
+def all_pilots_standings_by_race_by_season(race_name, season):
+    query = """
+    PREFIX driver: <http://f1/driver/pred/>
+    PREFIX driver_standing: <http://f1/driver_standing/pred/>
+    PREFIX race: <http://f1/race/pred/> 
+
+    SELECT ?code ?forename ?surname ?nationality ?season ?race_name ?round ?grid ?status ?position ?points WHERE
+    {   
+        ?driver_id driver:code ?code.
+        ?driver_id driver:forename ?forename.
+        ?driver_id driver:surname ?surname.
+        ?driver_id driver:nationality ?nationality.
+        ?driver_id driver:has ?driver_standing.
+
+        ?driver_standing driver_standing:grid ?grid.
+        ?driver_standing driver_standing:status ?status.
+        ?driver_standing driver_standing:position ?position.
+        ?driver_standing driver_standing:points ?points.
+        ?driver_standing driver_standing:at ?race_id. 
+
+        ?race_id race:name ?race_name.
+        ?race_id race:season ?season.
+        ?race_id race:round ?round.
+
+        FILTER (regex(?season, "SEASON_YEAR" , "i") && regex(?race_name, "RACE_NAME", "i"))
+
+    }
+
+    ORDER BY ASC( xsd:long ( STR(?position) ) )
+    
+    """
+
+    query = query.replace("SEASON_YEAR", str(season))
+    query = query.replace("RACE_NAME", race_name)
+
+    payload_query = {"query": query}
+    response = accessor.sparql_select(body=payload_query, repo_name=repo)
+    #print(response)
+    response = json.loads(response)
+
+    if response['results']['bindings']:
+        data = response['results']['bindings']
+        return [(pilot_info['code']['value'], 
+                 pilot_info['forename']['value'], 
+                 pilot_info['surname']['value'], 
+                 pilot_info['nationality']['value'],
+                 pilot_info['season']['value'],
+                 pilot_info['race_name']['value'],
+                 pilot_info['round']['value'],
+                 pilot_info['grid']['value'], 
+                 pilot_info['status']['value'],
+                 pilot_info['position']['value'], 
+                 pilot_info['points']['value']) for pilot_info in data]
+    else:
+        return []
+
+
+
 
 
 #print(races_by_season(2019))
 #print(pilot_standings_races_by_season("HAM", 2019))
+#print(all_pilots_standings_by_race_by_season("Australian Grand Prix", 2019))
