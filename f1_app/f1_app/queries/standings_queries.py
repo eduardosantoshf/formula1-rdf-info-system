c0061adb49aa5277c9a9e2fdb02fced6bb95cdab
bastos-01 <p.bastos112@gmail.com>
2023-03-28 16:09:06 +0100

    more queries

--- f1_app/f1_app/queries/standings_queries.py
+++ f1_app/f1_app/queries/standings_queries.py
@@ -223,7 +223,127 @@ def team_total_championships(name):
 
 
 
+""" Get final standings of all pilots for a specific season
+
+    Parameters
+    ----------
+    season : int
+        Season year
+
+    Returns
+    -------
+    list : tuples
+        [(driver_code, forename, surname, nationality, season, position, points)]
+    """
+def pilots_season_final_standings(season):
+    query = """
+    PREFIX driver: <http://f1/driver/pred/> 
+    PREFIX driver_final_standings: <http://f1/driver_final_standing/pred/>
+
+    SELECT ?driver_code ?forename ?surname ?nationality ?season ?position ?points WHERE
+    {   
+        ?driver_id driver:code ?driver_code.
+        ?driver_id driver:nationality ?nationality.
+        ?driver_id driver:forename ?forename.
+        ?driver_id driver:surname ?surname.
+
+        ?driver_id driver:finished_in ?dfs.
+        ?dfs driver_final_standings:season ?season.
+        ?dfs driver_final_standings:position ?position.
+        ?dfs driver_final_standings:points ?points.
+
+        FILTER regex(?season, "SEASON_YEAR" , "i")
+
+    }
+
+    ORDER BY ASC( xsd:long ( STR(?position) ) )
+    
+    """
+
+    query = query.replace("SEASON_YEAR", str(season))
+
+    payload_query = {"query": query}
+    response = accessor.sparql_select(body=payload_query, repo_name=repo)
+    #print(response)
+    response = json.loads(response)
+
+    if response['results']['bindings']:
+        data = response['results']['bindings']
+        return [(pilot['driver_code']['value'], 
+                 pilot['forename']['value'], 
+                 pilot['surname']['value'], 
+                 pilot['nationality']['value'],
+                 pilot['season']['value'],
+                 pilot['position']['value'],
+                 pilot['points']['value']) for pilot in data]
+    else:
+        return []
+
+
+
+""" Get final standings of all teams for a specific season
+
+    Parameters
+    ----------
+    season : int
+        Season year
+
+    Returns
+    -------
+    list : tuples
+        [(team_name, team_nationality, season, position, points)]
+    """
+def teams_season_final_standings(season):
+    query = """
+    PREFIX team: <http://f1/team/pred/> 
+    PREFIX team_final_standings: <http://f1/team_final_standing/pred/>
+
+    SELECT ?team_name ?team_nationality ?season ?position ?points WHERE
+    {   
+        ?team_id team:name ?team_name.
+        ?team_id team:nationality ?team_nationality.
+
+        ?team_id team:finished_in ?tfs.
+        ?tfs team_final_standings:season ?season.
+        ?tfs team_final_standings:position ?position.
+        ?tfs team_final_standings:points ?points.
+
+        FILTER regex(?season, "SEASON_YEAR" , "i")
+
+    }
+
+    ORDER BY ASC( xsd:long ( STR(?position) ) )
+    
+    """
+
+    query = query.replace("SEASON_YEAR", str(season))
+
+    payload_query = {"query": query}
+    response = accessor.sparql_select(body=payload_query, repo_name=repo)
+
+    response = json.loads(response)
+
+    if response['results']['bindings']:
+        data = response['results']['bindings']
+        return [(team['team_name']['value'],
+                 team['team_nationality']['value'],
+                 team['season']['value'],
+                 team['position']['value'],
+                 team['points']['value']) for team in data]
+    else:
+        return []
+
+
+
+
+
+
+
 #print(pilot_final_standings_by_season("HAM"))
 #print(team_final_standings_by_season("Mercedes"))
+
 #print(pilot_total_championships("HAM"))
 #print(team_total_championships("Mercedes"))
+
+#print(pilots_season_final_standings(2022))
+#print(teams_season_final_standings(2021))
