c48d0f0866f3bc4ea83b183e84fe26b3701ae814
bastos-01 <p.bastos112@gmail.com>
2023-03-29 23:20:43 +0100

    added locations for each race

--- f1_app/app/views.py
+++ f1_app/app/views.py
@@ -5,41 +5,41 @@ from f1_app.queries import standings_queries
 # Create your views here.
 
 cities = {
-    "Australian Grand Prix"
-    "Bahrain Grand Prix"
-    "Chinese Grand Prix"
-    "Azerbaijan Grand Prix"
-    "Spanish Grand Prix"
-    "Monaco Grand Prix"
-    "Canadian Grand Prix"
-    "French Grand Prix"
-    "Austrian Grand Prix"
-    "British Grand Prix"
-    "German Grand Prix"
-    "Hungarian Grand Prix"
-    "Belgian Grand Prix"
-    "Italian Grand Prix"
-    "Singapore Grand Prix"
-    "Russian Grand Prix"
-    "Japanese Grand Prix"
-    "Mexican Grand Prix"
-    "United States Grand Prix"
-    "Brazilian Grand Prix"
-    "Abu Dhabi Grand Prix"
-    "Styrian Grand Prix"
-    "70th Anniversary Grand Prix"
-    "Tuscan Grand Prix"
-    "Eifel Grand Prix"
-    "Portuguese Grand Prix"
-    "Emilia Romagna Grand Prix"
-    "Turkish Grand Prix"
-    "Sakhir Grand Prix"
-    "Dutch Grand Prix"
-    "Mexico City Grand Prix"
-    "São Paulo Grand Prix"
-    "Qatar Grand Prix"
-    "Saudi Arabian Grand Prix"
-    "Miami Grand Prix"
+    "Australian Grand Prix" : "Melbourne, Australia",
+    "Bahrain Grand Prix" : "Sakhir, Bahrain",
+    "Chinese Grand Prix" : "Shanghai, China",
+    "Azerbaijan Grand Prix" : "Baku, Azerbaijan",
+    "Spanish Grand Prix" : "Montmeló, Spain",
+    "Monaco Grand Prix" : "Monaco, France",
+    "Canadian Grand Prix" : "Montreal, Canada",
+    "French Grand Prix" : "Le Castellet, France",
+    "Austrian Grand Prix" : "Spielberg, Austria",
+    "British Grand Prix" : "Silverstone, UK",
+    "German Grand Prix" : "Nürburg, Germany",
+    "Hungarian Grand Prix" : "Budapest, Hungaria",
+    "Belgian Grand Prix" : "Ardennes, Belgium",
+    "Italian Grand Prix" : "Milan, Italy",
+    "Singapore Grand Prix" : "Marina Bay, Singapore",
+    "Russian Grand Prix" : "Sochi Autodrom, Russia",
+    "Japanese Grand Prix" : "Shizuoka, Japan",
+    "Mexican Grand Prix" : "Mexico City, Mexico",
+    "United States Grand Prix" : "Austin, Texas, USA",
+    "Brazilian Grand Prix" : "São Paulo, Brazil",
+    "Abu Dhabi Grand Prix" : "Yas Island, UAE",
+    "Styrian Grand Prix" : "Styria, Austria",
+    "70th Anniversary Grand Prix" : "Silverstone, UK",
+    "Tuscan Grand Prix" : "Tuscany, Italy",
+    "Eifel Grand Prix" : "Nürburg, Germany",
+    "Portuguese Grand Prix" : "Portimão, Portugal",
+    "Emilia Romagna Grand Prix" : "Emilia Romagna, Italy",
+    "Turkish Grand Prix" : "Istambul, Turkey",
+    "Sakhir Grand Prix" : "Sakhir, Bahrain",
+    "Dutch Grand Prix" : "Amsterdam, Netherlands",
+    "Mexico City Grand Prix" : "Mexico City, Mexico",
+    "São Paulo Grand Prix" : "São Paulo, Brazil",
+    "Qatar Grand Prix" : "Doha, Qatar",
+    "Saudi Arabian Grand Prix" : "Jeddah, Saudi Arabia",
+    "Miami Grand Prix" : "Florida, USA"
 }
 
 def results(request, season):
@@ -57,9 +57,13 @@ def teams(request):
 
 def races(request, season):
     races = races_queries.races_by_season(season)
-    print(races)
-    if len(races):
-        data = {'data': races}
+    new_races = []
+    for race in races:
+        new_races.append((race[0], race[1], cities[race[1]], race[3]))
+
+    print(new_races)
+    if len(new_races):
+        data = {'data': new_races}
     else:
         data = {'error': True}
         print("error")
