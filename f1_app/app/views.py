f3f5dc0fb9c6d99bdae428a7f593d7ee6f263d4c
Carina Neves <Carina.Neves@ctw.bmwgroup.com>
2023-03-29 16:32:35 +0100

    Add base frontend

--- f1_app/app/views.py
+++ f1_app/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def results(request):
+    return render(request, "results.html")
+
+def teams(request):
+    return render(request, "teams.html")
