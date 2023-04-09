547cb56729e450b0b74e945ed25233d96842c313
Eduardo Santos <eduardosantoshf@gmail.com>
2023-04-09 20:54:27 +0100

    Queries to create teams and contracts - needs some fixing

--- f1_app/f1_app/queries/admin_queries.py
+++ f1_app/f1_app/queries/admin_queries.py
@@ -2,7 +2,7 @@
 # @Author: Eduardo Santos
 # @Date:   2023-04-08 17:46:26
 # @Last Modified by:   Eduardo Santos
-# @Last Modified time: 2023-04-09 20:48:25
+# @Last Modified time: 2023-04-09 20:49:26
 
 import json
 from s4api.graphdb_api import GraphDBApi
@@ -126,3 +126,112 @@ def delete_contract(driver, team, season):
     )
 
 
+""" Create driver
+
+    Parameters
+    ----------
+    forename : str
+        Driver's forename
+    surname : str
+        Driver's surname
+    nationality : str
+        Driver's nationality
+    code : str
+        Driver's code
+    """
+def create_pilot(forename, surname, nationality, code):
+
+    #TODO: generate id to driver
+    
+    query = f"""
+    PREFIX driver: <http://f1/driver/pred/>
+
+    INSERT DATA {{
+        _:driver driver:forename "{forename}" ;
+                 driver:surname "{surname}" ;
+                 driver:nationality "{nationality}" ;
+                 driver:code "{code}" .
+    }}
+    """
+
+
+    response = requests.post(
+        f"{endpoint}/repositories/{repo}/statements",
+        headers = headers,
+        data = query
+    )
+
+
+#create_pilot("Santos", "Edu", "Portuguese", "SAN")
+
+
+""" Create team
+
+    Parameters
+    ----------
+    name : str
+        Team's name
+    nationality : str
+        Team's nationality
+    """
+def create_team(name, nationality):
+    
+    #TODO: generate id to team
+
+    query = f"""
+    PREFIX team: <http://f1/team/pred/>
+
+    INSERT DATA {{
+        _:team team:name "{name}" ;
+                 team:nationality "{nationality}" 
+    }}
+    """
+
+
+    response = requests.post(
+        f"{endpoint}/repositories/{repo}/statements",
+        headers = headers,
+        data = query
+    )
+    print(response)
+
+#create_team("EI", "Portuguese")
+
+
+""" Create contract
+
+    Parameters
+    ----------
+    driver : str
+        Driver's name
+    team : str
+        Team's name
+    season : int
+        Season
+    """
+def create_contract(driver, team, season):
+    
+    #TODO: generate id to team
+
+    query = f"""
+    PREFIX contract: <http://f1/contract/pred/>
+
+    INSERT DATA {{
+        _:contract contract:driver "{driver}" ;
+                 contract:team "{team}" ;
+                 contract:year "{season}" 
+    }}
+    """
+
+
+    response = requests.post(
+        f"{endpoint}/repositories/{repo}/statements",
+        headers = headers,
+        data = query
+    )
+    print(response)
+
+a = get_pilot_info("Santos")
+print(a)
+create_contract("1", "214", 2023)
+#create_contract("genid-e61b5268ba1f4122b3f9d8359203b0c917483-driver", "_:genid-e61b5268ba1f4122b3f9d8359203b0c920189-team", 2023)
